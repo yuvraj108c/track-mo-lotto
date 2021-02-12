2443ea86d090b9c7df3089cd80b5660824ca295c
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2021-02-12 10:26:09 +0400

    splash screen

---
+++ app/src/main/java/com/example/track_mo_lotto/Activities/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.example.track_mo_lotto.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.track_mo_lotto.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+}
