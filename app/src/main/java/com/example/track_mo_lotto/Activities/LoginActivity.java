b45e521b641e3a8a067108778f167c39e972f71f
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2021-02-12 11:43:45 +0400

    redirect to login

---
+++ app/src/main/java/com/example/track_mo_lotto/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.track_mo_lotto.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.track_mo_lotto.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/track_mo_lotto/Activities/SplashActivity.java
+++ app/src/main/java/com/example/track_mo_lotto/Activities/SplashActivity.java
@@ -1,12 +1,42 @@
 package com.example.track_mo_lotto.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.track_mo_lotto.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Button trackBtn, trackerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        trackBtn = findViewById(R.id.trackBtn);
+        trackerBtn = findViewById(R.id.trackerBtn);
+
+        trackBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        trackerBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
 
 
 }
