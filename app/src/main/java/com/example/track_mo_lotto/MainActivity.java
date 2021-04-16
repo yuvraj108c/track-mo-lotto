fff9d5af5585a6e31140566baec58c97ed69201c
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2021-04-16 20:04:02 +0400

    fix merge

--- app/src/main/java/com/example/track_mo_lotto/MainActivity.java
+++ app/src/main/java/com/example/track_mo_lotto/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.track_mo_lotto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
-=======
 import android.content.Intent;
->>>>>>> prefs
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_splash);
-    }
-=======
         setContentView(R.layout.activity_maps);
 //        startActivity(new Intent(MainActivity.this, com.example.track_mo_lotto.Activities.TrackingActivity.class));
     }
 
->>>>>>> prefs
 }
